dfd807b31f33ba6035cd63d29e26ed2f69747982
Ondina <47330080+ondina-draia@users.noreply.github.com>
2019-03-05 00:05:46 +0100

    Version corrigée qui fonctionne
    Lié les méthodes insérer et créer du contrôleur aux méthodes insérer et créer du modèle afin de pouvoir travailler sur le dictionnaire global

--- POO1/TP4/Annuaire/cmdControleurAnnuaire.py
+++ POO1/TP4/Annuaire/cmdControleurAnnuaire.py
@@ -5,30 +5,39 @@ from modeleAnnuaire import ModeleAnnuaire
 import cmdVueAnnuaire as vue
 import tkinter.messagebox
 
-repertoire = {} #le dictionnaire qui va contenir toutes les données de notre application annuaire
-#data = vue.installerComposants(vue.champPrenom, vue.champTelephone, vue.champAdresse, vue.champVille) #afin de remplir le répertoire, on ajoute les informations complémentaires au nom
+
+repertoire_global = ModeleAnnuaire() #le dictionnaire qui va contenir toutes les données de notre application annuaire
 
 def chercher(Nom):
     '''comand for button "chercher"'''
+    nom = Nom.get()
     if not Nom.get(): #tester si Nom est vide
         vue.afficheWarning("Champ \"Nom\" vide: Vous devez au minimum spécifier un nom")
         return False
-    elif not repertoire.estPresent(Nom):
+    elif not repertoire_global.estPresent(nom):
         vue.afficheWarning(Nom.get() + ": absent du répertoire")
         return False
     else:
-        print('test')
-    #repertoire.chercher(vue.Nom, data)
-
+        nom = Nom.get()
+        prenom, tel, adresse, ville = repertoire_global.chercher(nom)
+        print( prenom + ' ' + nom + " (Tél. " + tel + ") " + adresse + ", " + ville)
+        return True
+        
 def inserer(Nom, Prenom, Telephone, Adresse, Ville):
     '''comand for button "inserer"'''
     if not Nom.get(): #tester si nom est vide
         vue.afficheWarning("Champ \"Nom\" vide: vous devez au minimum spécifier un nom")
         return False
     else:
-        repertoire = Nom.get(), Prenom.get(), Telephone.get(), Adresse.get(), Ville.get()
-        tkinter.messagebox.showinfo(Nom.get() +": ajouté au répertoire")
+        data = ( Prenom.get(), Telephone.get(), Adresse.get(), Ville.get())
+        nom = Nom.get() 
+        repertoire = ModeleAnnuaire()
+        repertoire.inserer(nom, data)
+        tkinter.messagebox.showinfo(nom +": ajouté au répertoire")
         supprimer(Nom, Prenom, Telephone, Adresse, Ville)
+        global repertoire_global #appel de la variable globale afin de la modifier
+        repertoire_global.inserer(nom, data)
+        #print(repertoire_global)
         return repertoire
 
 def supprimer(Nom, Prenom, Telephone, Adresse, Ville):
@@ -43,74 +52,8 @@ def effacer(Nom, Prenom, Telephone, Adresse, Ville):
     '''comand for button "effacer"'''
     if tkinter.messagebox.askyesno("Voulez-vous vraiment tout effacer" + '\u00A0' + '?'):
         supprimer(Nom, Prenom, Telephone, Adresse, Ville)
+               
 
-#def chercher(repertoire, nom):
-#	"""
-#	Strip name and return the information associated with name
-#
-#	@param repertoire: repertoire to read from
-#	@type repertoire: dict
-#	@param name: name to look for
-#	@type name: string
-#	@return: success/failure
-#	@rtype: bool
-#	"""
-#	nom = nom.strip()
-#	if nom == '':
-#		vue.afficheWarning("Champ \"Nom\" vide: Vous devez au minimum spécifier un nom")
-#		return False
-#	if not repertoire.estPresent(nom):
-#		vue.afficheWarning(nom + ": absent du répertoire")
-#		return False
-#	prenom, tel, adresse, ville = repertoire.chercher(nom)
-#	vue.afficheMessage(prenom + ' ' + nom + " (Tél. " + tel + ") " + adresse + ", " + ville)
-#	return True
+        
 
-#def commande_inconnue(commande):
-#	vue.afficheWarning("commande inconnue: " + commande)
-
-#def exit():
-#	vue.afficheMessage("Au revoir")
-#	sys.exit(0)
-
-#def inserer(repertoire, reponse):
-#	"""
-#	Read reponse; If correctly formated, insert (firstname, tel, address, city) into repertoire[name]; Strip everything beforehand
-#
-#	@param repertoire: repertoire to insert into
-#	@type repertoire: dict
-#	@param reponse: 5-word string slash(/)-separated: "name / firstname / tel / address / city"
-#	@type reponse: string
-#	@return: success/failure
-#	@rtype: bool
-#	"""
-#	coordonnees = reponse.split("/")
-#	if len(coordonnees) != 5:
-#		vue.afficheWarning("trop ou pas assez d'infos (5 infos demandées : nom/prenom/tel/adresse/ville )")
-#		return False
-#	nom, prenom, tel, address, ville = coordonnees
-#	nom = nom.strip()
-#	if nom == '':
-#		vue.afficheWarning("Champ \"Nom\" vide: vous devez au minimum spécifier un nom")
-#		return False
-#	data = (prenom.strip(), tel.strip(), address.strip(), ville.strip())
-#	repertoire.inserer(nom, data)
-#	vue.afficheMessage(nom + " ajouté au répertoire")
-#	return True
-
-#if __name__ == '__main__':
-#	repertoire = ModeleAnnuaire()
-#	print("Commandes:\n- Insérer:  +nom/prenom/tel/adresse/ville\n- Chercher: ?nom\n- Quitter:  !")
-#	while True:
-#		reponse = input("> ")
-#		if len(reponse) > 0:
-#			commande = reponse.lstrip()[0]
-#			if commande == '!':
-#				exit()
-#			elif commande == '+':
-#				inserer(repertoire, reponse[1:])
-#			elif commande == '?':
-#				chercher(repertoire, reponse[1:])
-#			else:
-#				commande_inconnue(commande)
-
+        
