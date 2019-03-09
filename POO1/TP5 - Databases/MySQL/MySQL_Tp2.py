02cb9ca85c03de5339a7703d8cff63c7c22c8871
madeleine-murray <47334370+madeleine-murray@users.noreply.github.com>
2019-03-09 13:46:40 +0100

    Question 4 corrigée (je pense?)

--- POO1/TP5 - Databases/MySQL/MySQL_Tp2.py
+++ POO1/TP5 - Databases/MySQL/MySQL_Tp2.py
@@ -1,83 +1,89 @@
 import mysql.connector
- 
+
+#Se connecter a la base de donnees 
 monServeur = "ensembldb.ensembl.org" 
 monLogin = "anonymous" 
 connexion = mysql.connector.connect(host = monServeur, user = monLogin) 
 curseur = connexion.cursor() # permet d'effectuer des requetes 
 
+#verifie la version utilisee
 curseur.execute("select version()") # ne retourne qu’une seule ligne 
 row = curseur.fetchone() # on recupere la ligne resultat 
 print("Version du serveur MySQL distant : ", row[0]) 
 
+#verifie le serveur utilise
 curseur.execute("select now()") # ne retourne qu’une seule ligne 
 row = curseur.fetchone() # on recupere la ligne resultat 
 print("Date actuelle sur le serveur distant : ", row[0]) 
 
-
+#verifie la date de connexion
 curseur.execute("select user()") # ne retourne qu’une seule ligne 
 row = curseur.fetchone() # on recupere la ligne resultat 
 print(type(row)) #fetchone c'est un tuple (une ligne de la table)
 print("Date actuelle sur le serveur distant : ", row[0]) 
 
-
+#verifie la base de donnee selectionee: ici aucune pour l'instant
 curseur.execute("select database()") # ne retourne qu’une seule ligne 
 row = curseur.fetchone() # on recupere la ligne resultat 
 print("Base de donnée selectionnée : ", row[0]) 
 
-#Question 2
+
+## QUESTION 2
+
+#affiche toutes les bases de donnes presentes
 curseur.execute("show databases") # retourne plusieurs lignes 
 rows = curseur.fetchall() # on recupere les lignes resultat 
 print(type(rows)) #fetchall c'est une liste de tuple (toute la table)
 for row in rows:
-    print(row[0]) 
+    print("Liste des bases de données: ", row[0]) 
 
-curseur.execute("show databases like '%homo_sapiens_core%'") # retourne plusieurs lignes 
+#affiche toutes les bases de donnes contenant "homo_sapiens_core_"
+curseur.execute("show databases like '%homo_sapiens_core_%'") # retourne plusieurs lignes 
 rows = curseur.fetchall() # on recupere les lignes resultat 
 print(type(rows)) #fetchall c'est une liste de tuple (toute la table)
 for row in rows:
-    print(row[0])
+    print("Bases de données intéressantes: ", row[0])
 
-##Question 3
-#Je ne sais pas comment sélectionner de façon automatique la derniere version de la base de donnee
+
+## QUESTION 3
+
+#affiche la base de donnee Humaine la plus recente
 curseur.execute("use homo_sapiens_core_95_38") #exécuter la base qu'on veut selectionner
 
 curseur.execute("select database()") # ne retourne qu’une seule ligne 
 row = curseur.fetchone() # on recupere la ligne resultat 
 print("Base de donnée selectionnée : ", row[0])  #mtn ça affiche qu'elle est la base de donnee qui est selectionnée
 
+# Pour selectionnner la base de donnee de facon automatique, on avait pense a faire une boucle:
+# depuis la liste de toutes les "bases de donnees interessantes" on voulait selectionner la derniere
+# (soit la plus recente) mais cela ne marchait pas car il s'agissait d'un tuple.
 
-#Question 4 il faut utiliser gene-id et transcript_id (clé primaire)
+
+## QUESTION 4 il faut utiliser gene-id et transcript_id (clé primaire)
+
+#affiche toutes les tables de notre base de donnee
 curseur.execute("show tables") # retourne plusieurs lignes 
 rows = curseur.fetchall() # on recupere les lignes resultat 
 print(type(rows)) #fetchall c'est une liste de tuple (toute la table)
 for row in rows:
-    print(row[0]) 
+    print("Liste tables: ", row[0]) 
 
-#Decrit le contenu de la table gene
-curseur.execute("describe gene") # retourne plusieurs lignes 
-rows = curseur.fetchall() # on recupere les lignes resultat 
-print(type(rows)) #fetchall c'est une liste de tuple (toute la table)
-for row in rows:
-    print(row[0])   
-
-#Decrit le contenu de la table transcript
+##decrit le contenu de la table gene
 curseur.execute("describe transcript") # retourne plusieurs lignes 
 rows = curseur.fetchall() # on recupere les lignes resultat 
 print(type(rows)) #fetchall c'est une liste de tuple (toute la table)
 for row in rows:
-    print(row[0])  
+    print("Table transcript: ", row[0])
 
-#Lister tous les transcripts du gene ayant le plus de transcripts
-#Probleme avec ça, je ne suis pas certaine de la syntaxe et d'avoir compris ce qu'il demande d'autant plus que le programme ne tourne pas
-curseur.execute("select transcript_id, gene_id from transcript where gene_id= (select MAX(transcript_id) from transcript);") # retourne plusieurs lignes 
+#selectionne le gene avec le plus de transcrit
+curseur.execute("select transcript_id from transcript where gene_id = (select max(gene_id) from transcript)") # retourne plusieurs lignes 
 rows = curseur.fetchall() # on recupere les lignes resultat 
 print(type(rows)) #fetchall c'est une liste de tuple (toute la table)
 for row in rows:
-    print(row[0])  
+    print("Nombre de transcrits: ", row[0]) 
 
 
-#Question 5
+## QUESTION 5
 
 curseur.close()
 connexion.close()
-
